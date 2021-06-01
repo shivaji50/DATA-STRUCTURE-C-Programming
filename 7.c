dce4286e2960b5db13be92e251d559d04910c024
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-06-02 01:28:29 +0530

    a program which display largest digits of all element from singly  linear linked list.

---
+++ 7.c
@@ -0,0 +1,82 @@
+// Function Prototype : void Maximum( PNODE Head); 
+// Input linked list : |11|->|250|->|532|->|419| 
+// Output : 1 5 5 9
+// author : Shivai Das
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+   int Data;
+   struct node *Next;
+};
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node ** PPNODE;
+void InsertL(PPNODE Head,int No)
+{
+	PNODE newn=NULL;
+	PNODE Temp=*Head;
+	newn=(PNODE)malloc(sizeof(NODE));
+	newn->Data=No;
+	newn->Next=NULL;
+
+	if(*Head==NULL)
+	{
+		*Head=newn;
+	}
+	else
+	{
+		while(Temp->Next!=NULL)
+		{
+			Temp=Temp->Next;
+        }
+        Temp->Next=newn;
+	}
+}
+void Display(PNODE Head)
+{
+	while(Head!=NULL)
+	{
+		printf("|%d|\t",Head->Data);
+		Head=Head->Next;
+	}
+	printf("\n\n");
+}
+void Maximum(PNODE Head)
+{
+	int idigit=0,pal=0,Max=0;
+	while(Head!=NULL)
+	{
+		pal=0;
+		Max=Head->Data;
+		while(Max>0)
+		{
+			idigit=Max%10;
+			
+			if(pal<idigit)
+			{pal=idigit;}
+
+			Max=Max/10;
+		}
+		
+			printf("%d\t",pal);
+		   Head=Head->Next;
+	}
+}
+int main()
+{
+	PNODE First=NULL;
+	int ret=0;
+
+	InsertL(&First,11);
+	InsertL(&First,28);
+	InsertL(&First,70);
+	InsertL(&First,419);
+	InsertL(&First,6);
+	
+	Display(First);
+	Maximum(First);
+	
+    return 0;	
+}
